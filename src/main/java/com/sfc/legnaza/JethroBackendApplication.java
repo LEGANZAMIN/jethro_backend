423ead313de30c13edfafb75db22f0bb412ef117
LeeSungmin <lsmin01@gmail.com>
2020-06-10 22:24:29 +0900

    Initial Commit

---
+++ src/main/java/com/sfc/legnaza/JethroBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sfc.legnaza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JethroBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JethroBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sfc/legnaza/JethroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfc.legnaza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JethroBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
